a125a7290d01cfc401ad43b0d9226081ca38ab58
Sanket Korgaonkar <sanket.korgaonkar@gmail.com>
2017-06-02 14:20:11 -0500

    Added reinforcement learning and hmm for my lazyBones class!! LOL

--- advanced.matrix/src/main/java/learning/solutions/advanced/matrix/engineeringLevel/AdjacencyCalculator.java
+++ advanced.matrix/src/main/java/learning/solutions/advanced/matrix/engineeringLevel/AdjacencyCalculator.java
@@ -1,71 +1,124 @@
 package learning.solutions.advanced.matrix.engineeringLevel;
 
-import java.awt.Point;
-import java.util.Map;
-import java.util.Properties;
-
 import learning.solutions.advanced.matrix.domain.NavCell;
-import learning.solutions.advanced.matrix.domain.NavCell.Direction;
+import learning.solutions.advanced.matrix.domain.RCell;
 import learning.solutions.advanced.matrix.domain.Wall;
 import learning.solutions.advanced.matrix.domain.WallBuilder;
 import learning.solutions.advanced.matrix.utils.EnvironmentUtils;
 import learning.solutions.advanced.matrix.utils.NavUtil;
+import learning.solutions.advanced.matrix.utils.ReinforcementLearnerUtil;
+
+import java.awt.*;
+import java.util.Map;
+import java.util.Properties;
 
 public class AdjacencyCalculator {
-	private static final int		VALID_DIR		= 4;
-	private int						cellWidth		= -1;
-	private int						frameHeight		= -1;
-	private int						frameWidth		= -1;
-	private Point[]					adjNodes		= new Point[VALID_DIR];
-	private NavCell[]				adjacentNodes	= new NavCell[VALID_DIR];
-	private Map<Integer, NavCell>	gridMap			= null;
-	private WallBuilder				wallBuilder		= null;
+    private static final int                   VALID_DIR      = 4;
+    private              int                   cellWidth      = -1;
+    private              int                   frameHeight    = -1;
+    private              int                   frameWidth     = -1;
+    private              Point[]               adjNodes       = new Point[VALID_DIR];
+    private              NavCell[]             adjacentNodes  = new NavCell[VALID_DIR];
+    private              RCell[]               adjacentRNodes = new RCell[VALID_DIR];
+    private              Map<Integer, NavCell> gridMap        = null;
+    private              RCell[][]             rGridMap       = null;
+    private              WallBuilder           wallBuilder    = null;
+    private              int                   lidarCost      = 0;
 
-	public AdjacencyCalculator(Point center, Properties matrixConfig) {
-		this.gridMap = NavUtil.populateGridMap(matrixConfig);
-		this.wallBuilder = new WallBuilder(matrixConfig);
-		
-		cellWidth = Integer.parseInt(matrixConfig.getProperty(EnvironmentUtils.CELL_WIDTH_PROPERTY));
-		frameHeight = Integer.parseInt(matrixConfig.getProperty(EnvironmentUtils.FRAME_HEIGHT_PROPERTY));
-		frameWidth = Integer.parseInt(matrixConfig.getProperty(EnvironmentUtils.FRAME_WIDTH_PROPERTY));
-		adjNodes[Direction.NORTH.getValue()] = new Point(center.x, center.y - cellWidth);
-		adjNodes[Direction.SOUTH.getValue()] = new Point(center.x, center.y + cellWidth);
-		adjNodes[Direction.EAST.getValue()] = new Point(center.x + cellWidth, center.y);
-		adjNodes[Direction.WEST.getValue()] = new Point(center.x - cellWidth, center.y);
-	}
+    private double rlMinReward = 0.0d;
+    private double rlMaxReward = 0.0d;
 
-	/**
-	 * Returns a valid NavCell for viable navigation and null for invalid
-	 * navigation. For the order of the returned array please refer to
-	 * NavCell.Direction
-	 * 
-	 * @return
-	 */
-	public NavCell[] getAdjacentNodes() {
-		for (int i = 0; i < VALID_DIR; i++) {
-			Point temp = adjNodes[i];
-			if (temp.x < 0 || temp.x >= frameWidth || temp.y < 0 || temp.y >= frameHeight || intersectsWall(temp)) {
-				adjacentNodes[i] = null;
-				continue;
-			} else {
-				int id = NavUtil.findNavId(gridMap, temp);
-				NavCell nCell = gridMap.get(id);
-				adjacentNodes[i] = nCell;
-			}
-		}
+    public AdjacencyCalculator(Point center, Properties marsConfig) {
+        this.gridMap = NavUtil.populateGridMap(marsConfig);
+        this.wallBuilder = new WallBuilder(marsConfig);
 
-		return adjacentNodes;
-	}
+        cellWidth = Integer.parseInt(marsConfig.getProperty(EnvironmentUtils.CELL_WIDTH_PROPERTY));
+        frameHeight = Integer.parseInt(marsConfig.getProperty(EnvironmentUtils.FRAME_HEIGHT_PROPERTY));
+        frameWidth = Integer.parseInt(marsConfig.getProperty(EnvironmentUtils.FRAME_WIDTH_PROPERTY));
 
-	private boolean intersectsWall(Point temp) {
-		boolean intersects = false;
-		for (Wall w : wallBuilder.getWalls()) {
-			if (w.intersects(temp)) {
-				intersects = true;
-				break;
-			}
-		}
+        adjNodes[RCell.Direction.NORTH.getValue()] = new Point(center.x, center.y - cellWidth);
+        adjNodes[RCell.Direction.SOUTH.getValue()] = new Point(center.x, center.y + cellWidth);
+        adjNodes[RCell.Direction.EAST.getValue()] = new Point(center.x + cellWidth, center.y);
+        adjNodes[RCell.Direction.WEST.getValue()] = new Point(center.x - cellWidth, center.y);
 
-		return intersects;
-	}
+        rlMaxReward = Double.parseDouble(marsConfig.getProperty(ReinforcementLearner.RL_ENGINE_PREFIX + ".maxReward"));
+        rlMinReward = Double.parseDouble(marsConfig.getProperty(ReinforcementLearner.RL_ENGINE_PREFIX + ".minReward"));
+    }
+
+    public void setrGridMap(RCell[][] rGridMap) {
+        this.rGridMap = rGridMap;
+    }
+
+    /**
+     * Returns a valid NavCell for viable navigation and null for invalid
+     * navigation. For the order of the returned array please refer to
+     * NavCell.Direction
+     *
+     * @return
+     */
+    public NavCell[] getAdjacentNodes() {
+        for (int i = 0; i < VALID_DIR; i++) {
+            Point temp = adjNodes[i];
+            if (temp.x < 0 || temp.x >= frameWidth || temp.y < 0 || temp.y >= frameHeight || intersectsWall(temp)) {
+                adjacentNodes[i] = null;
+                continue;
+            } else {
+                int     id    = NavUtil.findNavId(gridMap, temp);
+                NavCell nCell = gridMap.get(id);
+                adjacentNodes[i] = nCell;
+            }
+        }
+
+        return adjacentNodes;
+    }
+
+    /**
+     * Returns a valid NavCell for viable navigation and null for invalid
+     * navigation. For the order of the returned array please refer to
+     * NavCell.Direction
+     *
+     * @return
+     */
+    public RCell[] getAdjacentRNodes() {
+        for (int i = 0; i < VALID_DIR; i++) {
+            Point temp = adjNodes[i];
+            if (temp.x < 0 || temp.x >= frameWidth || temp.y < 0 || temp.y >= frameHeight) {
+                adjacentRNodes[i] = null;
+                continue;
+            } else {
+                int id = ReinforcementLearnerUtil.findPoint(temp, rGridMap).getId();
+                adjacentRNodes[i] = new RCell(temp, id, cellWidth);
+
+                if (intersectsWall(temp)) {
+                    adjacentRNodes[i].setReward(rlMinReward);
+                } else {
+                    adjacentRNodes[i].setReward(rlMaxReward);
+                }
+            }
+        }
+
+        return adjacentRNodes;
+    }
+
+    public int getLidarCost() {
+        return lidarCost;
+    }
+
+    public void setLidarCost(int lidarCost) {
+        this.lidarCost = lidarCost;
+    }
+
+    private boolean intersectsWall(Point temp) {
+        boolean intersects = false;
+        for (Wall w : wallBuilder.getWalls()) {
+            if (w.intersects(temp)) {
+                intersects = true;
+                break;
+            }
+        }
+
+        lidarCost++;
+
+        return intersects;
+    }
 }
---
+++ advanced.matrix/src/main/java/learning/solutions/advanced/matrix/engineeringLevel/Oracle.java
@@ -0,0 +1,271 @@
+package learning.solutions.advanced.matrix.engineeringLevel;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by sanketkorgaonkar on 5/31/17.
+ */
+public class Oracle {
+    private int    numObservations           = 0;
+    private int    numStates                 = 0;
+    private int    lengthObservationSequence = 0;
+    private int    maxIterations             = 0;
+    private double numerator                 = 0.0d;
+    private double denominator               = 0.0d;
+    private double prevLogProb               = -100000.0d;
+
+    private double[]   initialStateDist = null;
+    private double[][] observationProb  = null;
+    private double[][] stateTransitions = null;
+
+    private double[][]   alpha        = null;
+    private double[][]   beta         = null;
+    private double[][][] gammaT       = null;
+    private double[][]   gammat       = null;
+    private double[]     scaleFactor  = null;
+    private int[]        observations = null;
+
+    /**
+     * @param numObservations
+     * @param numStates
+     * @param maxIterations
+     * @param observations
+     */
+    public Oracle(int numObservations, int numStates, int maxIterations, int[] observations) {
+        this.lengthObservationSequence = observations.length;
+        this.numObservations = numObservations;
+        this.numStates = numStates;
+        this.maxIterations = maxIterations;
+
+        initialStateDist = new double[numStates];
+        observationProb = new double[numStates][numObservations];
+        this.observations = observations;
+        stateTransitions = new double[numStates][numStates];
+
+        alpha = new double[lengthObservationSequence][numStates];
+        beta = new double[lengthObservationSequence][numStates];
+        gammaT = new double[lengthObservationSequence][numStates][numStates];
+        gammat = new double[lengthObservationSequence][numStates];
+        scaleFactor = new double[lengthObservationSequence];
+
+        // initialize matrices
+        fillArrays();
+        seedInitialStateDist();
+        initializeObservationMatrix();
+        initializeStateTransitions();
+
+        System.out.println("Printing the stateTransition matrix:: A :: ");
+        printMatrix(stateTransitions, numStates, numStates);
+
+        System.out.println("Printing the observationProb matrix:: B :: ");
+        printMatrix(observationProb, numStates, numObservations);
+
+        System.out.println("Printing the initialState matrix PI :: ");
+        System.out.println(Arrays.toString(initialStateDist));
+
+        trainHMM();
+    }
+
+    public double[] getInitialStateDist() {
+        return initialStateDist;
+    }
+
+    public double[][] getObservationProb() {
+        return observationProb;
+    }
+
+    public double[][] getStateTransitions() {
+        return stateTransitions;
+    }
+
+    public void setObservations(int[] observations) {
+        if (observations.length == lengthObservationSequence) {
+            this.observations = observations;
+        }
+    }
+
+    private void fillArrays() {
+        Arrays.fill(this.scaleFactor, 0.0d);
+        Arrays.fill(this.gammat, 0.0d);
+        Arrays.fill(this.initialStateDist, 0.0d);
+        Arrays.fill(this.alpha, 0.0d);
+        Arrays.fill(this.gammaT, 0.0d);
+        Arrays.fill(this.beta, 0.0d);
+    }
+
+    private void trainHMM() {
+        double currentProb = 0.0d;
+        int    i           = 0;
+        while ((currentProb > prevLogProb) && (i < maxIterations)) {
+            prevLogProb = currentProb;
+            alphaPass();
+            betaPass();
+            updateGammas();
+            reEstimateABPI();
+            currentProb = computeLogProb();
+            i++;
+        }
+    }
+
+    private void betaPass() {
+        for (int i = 0; i < numStates; i++) {
+            this.beta[lengthObservationSequence - 1][i] = scaleFactor[lengthObservationSequence - 1];
+        }
+
+        for (int t = lengthObservationSequence - 2; t < 0; t--) {
+            for (int i = 0; i < numStates; i++) {
+                for (int j = 0; j < numStates; j++) {
+                    beta[t][j] += stateTransitions[i][j] * observationProb[j][t + 1] * beta[t + 1][j];
+                }
+                beta[t][i] *= scaleFactor[t];
+            }
+        }
+    }
+
+    private void updateGammas() {
+        for (int t = 0; t < (lengthObservationSequence - 1); t++) {
+            for (int i = 0; i < numStates; i++) {
+                for (int j = 0; j < numStates; j++) {
+                    denominator += (alpha[t][i] * stateTransitions[i][j] * observationProb[j][t + 1] * beta[t + 1][j]);
+                }
+            }
+            for (int i = 0; i < numStates; i++) {
+                for (int j = 0; j < numStates; j++) {
+                    gammaT[t][i][j] = ((alpha[t][i] * stateTransitions[i][j] * observationProb[j][t + 1] * beta[t +
+                            1][j]) / denominator);
+                    gammat[t][i] += gammaT[t][i][j];
+                }
+            }
+        }
+
+        denominator = 0.0d;
+        for (int i = 0; i < numStates; i++) {
+            denominator += alpha[lengthObservationSequence - 1][i];
+        }
+
+        for (int i = 0; i < numStates; i++) {
+            gammat[lengthObservationSequence - 1][i] = alpha[lengthObservationSequence - 1][i] / denominator;
+        }
+    }
+
+    private void reEstimateABPI() {
+        for (int i = 0; i < numStates; i++) {
+            initialStateDist[i] = gammat[0][i];
+        }
+
+        for (int i = 0; i < numStates; i++) {
+            for (int j = 0; j < numStates; j++) {
+                for (int t = 0; t < (lengthObservationSequence - 1); t++) {
+                    numerator += gammaT[t][i][j];
+                    denominator += gammat[t][i];
+                }
+                stateTransitions[i][j] = numerator / denominator;
+            }
+        }
+
+        for (int i = 0; i < numStates; i++) {
+            for (int j = 0; j < lengthObservationSequence; j++) {
+                numerator = 0.0d;
+                denominator = 0.0d;
+                for (int t = 0; t < lengthObservationSequence; t++) {
+                    if (observations[t] == j) {
+                        numerator += gammat[t][i];
+                        denominator += gammat[t][i];
+                    }
+                }
+                observationProb[i][j] = numerator / denominator;
+            }
+        }
+    }
+
+    private double computeLogProb() {
+        double logProb = 0.0d;
+        for (int i = 0; i < lengthObservationSequence; i++) {
+            logProb += Math.log(scaleFactor[i]);
+        }
+        logProb *= -1.0d;
+        return logProb;
+    }
+
+    private void seedInitialStateDist() {
+        double maxPi = 1.0d / (double) numStates;
+        double sum   = 0.0d;
+        for (int i = 0; i < initialStateDist.length; i++) {
+            double temp = ThreadLocalRandom.current().nextDouble(maxPi);
+
+            if (i == (numStates - 1)) {
+                initialStateDist[i] = 1.0d - sum;
+                continue;
+            }
+            sum += temp;
+            initialStateDist[i] = temp;
+        }
+    }
+
+    private void initializeStateTransitions() {
+        double maxIS = 1.0d / (double) numStates;
+        for (int i = 0; i < stateTransitions[0].length; i++) {
+            double sum = 0.0d;
+            for (int j = 0; j < stateTransitions[0].length; j++) {
+                double temp = ThreadLocalRandom.current().nextDouble(maxIS);
+
+                if (j == (numStates - 1)) {
+                    stateTransitions[i][j] = 1.0d - sum;
+                    continue;
+                }
+                sum += temp;
+                stateTransitions[i][j] = temp;
+            }
+        }
+    }
+
+    private void initializeObservationMatrix() {
+        double maxOM = 1.0d / (double) numObservations;
+        for (int i = 0; i < numStates; i++) {
+            double sum = 0.0d;
+            for (int j = 0; j < numObservations; j++) {
+                double temp = ThreadLocalRandom.current().nextDouble(maxOM);
+
+                if (j == (numObservations - 1)) {
+                    observationProb[i][j] = 1.0d - sum;
+                    continue;
+                }
+                sum += temp;
+                observationProb[i][j] = temp;
+            }
+        }
+    }
+
+    private void printMatrix(double[][] matrix, int row, int col) {
+        for (int i = 0; i < row; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
+
+    private void alphaPass() {
+        for (int i = 0; i < numStates; i++) {
+            alpha[0][i] = this.initialStateDist[i] * this.observationProb[i][0];
+            scaleFactor[0] += alpha[0][i];
+        }
+        scaleFactor[0] = 1.0d / scaleFactor[0];
+
+        for (int i = 0; i < numStates; i++) {
+            alpha[0][i] *= scaleFactor[0];
+        }
+
+        for (int t = 1; t < lengthObservationSequence; t++) {
+            for (int i = 0; i < numStates; i++) {
+                for (int j = 0; j < numStates; j++) {
+                    alpha[t][i] += (alpha[t - 1][j] * stateTransitions[j][i]);
+                }
+                alpha[t][i] *= this.observationProb[i][t];
+                scaleFactor[t] += alpha[t][i];
+            }
+            scaleFactor[t] = 1.0d / scaleFactor[t];
+            for (int i = 0; i < numStates; i++) {
+                alpha[t][i] *= scaleFactor[t];
+            }
+        }
+    }
+}
